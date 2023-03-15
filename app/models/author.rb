3b3b8a0a1581ae8bfdf32145593907ed33d9462a
OyakMasu21 <akzaki17@gmail.com>
2023-03-15 13:05:07 +0300

    Completed assignment

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,12 @@
 class Author < ApplicationRecord
+    has_many :posts
+
+    # add this:
+    has_one :profile
+
 end
+
+class Profile < ApplicationRecord
+    # add this:
+    belongs_to :author
+  end
